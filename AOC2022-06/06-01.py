11e7a81801a10a390ec376c6b029b175cb52c526
Nolan <nolanwelch@outlook.com>
2022-12-06 15:12:37 -0500

    Day 6 and added READMEs to each day

---
+++ AOC2022-06/06-01.py
@@ -0,0 +1,20 @@
+input_file = open("input.txt")
+input_line = input_file.readlines()[0]
+
+WINDOW_SIZE = 4
+
+char_ind = 0
+window = []
+
+for char_ind, char in enumerate(input_line):
+    if char in window:
+        shift_start = window.index(char) + 1
+        window = window[shift_start:]
+    window.append(char)
+    char_ind += 1
+    if len(window) == WINDOW_SIZE:
+        break
+
+print(char_ind)
+
+input_file.close()
